a004ca34b5bd6da88ddb68f698d311fd62371587
avigunjal07 <avishkargunjal07@gmail.com>
2026-04-30 15:30:33 +0530

    simple working code is ready

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { Trash } from "lucide-react";
 
-const TaskCard = () => {
+function TaskCard({ task, deleteTask }) {
   return (
-    <div>
-      
+    <div className="
+      bg-white
+      my-3
+      mx-auto
+      px-5
+      py-4
+      flex
+      justify-between
+      items-center
+    ">
+      <span className="
+        text-lg
+        text-gray-800
+        font-medium
+      ">
+        {task}
+      </span>
+
+      <Trash
+        size={20}
+        className="
+          cursor-pointer
+          text-gray-400
+        "
+        onClick={() => deleteTask(task)}
+      />
     </div>
-  )
+  );
 }
 
-export default TaskCard
+export default TaskCard;
